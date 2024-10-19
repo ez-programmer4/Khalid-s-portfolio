c0f06883f094985b099d4ce5ae0d713e7c1bd1bc
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-10-19 06:11:05 +0300

    commiting after dev

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ function Contact() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("https://backend-portfolio-hjtx.onrender.com/send-email", {
+    fetch("https://backend-khalid-portfolio-hjtx.onrender.com/send-email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
